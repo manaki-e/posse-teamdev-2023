cd590f2b594e059b2267bd3d7077156940ebf5a3
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-04-27 16:19:29 +0000

    create admin dashboard

---
+++ src/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+<x-admin-app>
+    <x-slot name="title">
+            {{ __('ダッシュボード') }}
+    </x-slot>
+    <x-slot name="discription">
+            {{ __('ダッシュボード') }}
+    </x-slot>
+</x-admin-app>
