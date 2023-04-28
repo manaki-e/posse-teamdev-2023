8f4e06b0f4ca5668985540e3242ab1765daac243
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-04-28 01:27:50 +0000

    settingsテーブル作成

---
+++ src/database/migrations/2023_04_28_012231_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('monthly_distribution_point');
+            $table->integer('maximum_event_participate_point');
+            $table->timestamps();
+        });
+        DB::table('settings')->insert(['monthly_distribution_point'=>5000,'maximum_event_participate_point'=>500]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
