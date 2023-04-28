989d8e05690d212a979c6cb4a38c7e2038333e49
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-04-28 00:44:12 +0000

    product_tagにsoftdelete追加

--- src/database/migrations/2023_04_24_122147_create_product_tag_table.php
+++ src/database/migrations/2023_04_24_122147_create_product_tag_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained('products');
             $table->foreignId('tag_id')->constrained('tags')->where('request_type_id', '=', '1');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
