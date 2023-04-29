dadd27eb31858dd1e57f3a995e1052e85fdb4266
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-04-29 12:20:24 +0000

    app.phpから無駄な内容削除した

--- src/config/app.php
+++ src/config/app.php
@@ -194,7 +194,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -211,7 +210,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Image' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];
