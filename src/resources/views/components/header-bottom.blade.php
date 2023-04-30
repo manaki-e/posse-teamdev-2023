3bc34e31ab7dd76a5cc15f13b954abaff1c7eaa0
soh-github <94168577+soh-github@users.noreply.github.com>
2023-04-30 10:51:59 +0000

    ルーティングによって色が変化するよう修正

--- src/resources/views/components/header-bottom.blade.php
+++ src/resources/views/components/header-bottom.blade.php
@@ -1,12 +1,20 @@
+@php
+
+$selected = 'user-tab-text-green py-4 px-6 block border-b-4 font-bold user-border-green';
+$unselected = 'text-gray-600 py-4 px-6 block';
+
+@endphp
+
+
 <div class="container mx-auto flex flex-wrap flex-col md:flex-row items-center border-b-2">
     <nav class="flex flex-col sm:flex-row">
-        <button class="user-tab-text-green py-4 px-6 block focus:outline-none border-b-4 font-bold user-border-green">
+        <button class="{{ Request::routeIs('users.items.*') ? $selected : $unselected }}">
             <a href="#" class="">アイテム</a>
         </button>
-        <button class="text-gray-600 py-4 px-6 block focus:outline-none">
+        <button class="{{ Request::routeIs('users.requests.*) ? $selected : $unselected }}">
             <a href="#" class="">リクエスト</a>
         </button>
-        <button class="text-gray-600 py-4 px-6 block focus:outline-none">
+        <button class="{{ Request::routeIs('users.events.*') ? $selected : $unselected }}">
             <a href="#" class="">イベント</a>
         </button>
     </nav>
