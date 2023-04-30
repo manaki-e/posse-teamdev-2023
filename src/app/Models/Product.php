812deeb5d732cd662b0fe68fe2a3c154e03b85ad
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-04-30 11:04:28 +0000

    formatした

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -8,30 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    const STATUS=[
-        'pending'=>1,
-        'available'=>2,
-        'occupied'=>3
+    const STATUS = [
+        'pending' => 1,
+        'available' => 2,
+        'occupied' => 3
     ];
-    public function scopeGetProductIds($query){
+    public function scopeGetProductIds($query)
+    {
         return $query->pluck('id')->toArray();
     }
-    public function scopePendingProducts($query){
-        return $query->where('status',self::STATUS['pending']);
+    public function scopePendingProducts($query)
+    {
+        return $query->where('status', self::STATUS['pending']);
     }
-    public function scopeAvailableProducts($query){
-        return $query->where('status',self::STATUS['available']);
+    public function scopeAvailableProducts($query)
+    {
+        return $query->where('status', self::STATUS['available']);
     }
-    public function scopeOccupiedProducts($query){
-        return $query->where('status',self::STATUS['occupied']);
+    public function scopeOccupiedProducts($query)
+    {
+        return $query->where('status', self::STATUS['occupied']);
     }
-    public function scopeApprovedProducts($query){
-        return $query->where('status','!=',self::STATUS['pending']);
+    public function scopeApprovedProducts($query)
+    {
+        return $query->where('status', '!=', self::STATUS['pending']);
     }
-    public function deals(){
+    public function deals()
+    {
         return $this->hasMany(ProductDealLog::class);
     }
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 }
