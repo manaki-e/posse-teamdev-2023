aa13b8439d332cc05ee992c8b55d64ebbe4114ee
soh-github <94168577+soh-github@users.noreply.github.com>
2023-04-30 09:46:24 +0000

    header-bottom完了

---
+++ src/resources/views/components/header-bottom.blade.php
@@ -0,0 +1,13 @@
+<div class="container mx-auto flex flex-wrap flex-col md:flex-row items-center border-b-2">
+    <nav class="flex flex-col sm:flex-row">
+        <button class="user-tab-text-green py-4 px-6 block focus:outline-none border-b-4 font-bold user-border-green">
+            <a href="#" class="">アイテム</a>
+        </button>
+        <button class="text-gray-600 py-4 px-6 block focus:outline-none">
+            <a href="#" class="">リクエスト</a>
+        </button>
+        <button class="text-gray-600 py-4 px-6 block focus:outline-none">
+            <a href="#" class="">イベント</a>
+        </button>
+    </nav>
+</div>
