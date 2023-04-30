09b3bce6a08a8dfea2b7c50b2c204dbe53acf500
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-04-30 04:46:56 +0000

    admin button component

---
+++ src/resources/views/components/admin-button-delete.blade.php
@@ -0,0 +1,3 @@
+<button class="middle none center mr-3 rounded-lg border border-red-700 py-3 px-6 font-sans text-xs font-bold uppercase text-red-700 transition-all hover:opacity-75 focus:ring focus:ring-red-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-dark="true">
+    削除
+</button>
