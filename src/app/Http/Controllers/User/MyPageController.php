6ea81b9b0ff674c3b636674b86776d57f7968b56
IGARASHI <88939517+YoIgarashi@users.noreply.github.com>
2023-05-03 13:32:42 +0000

    自身の主催したイベント一覧取得

--- src/app/Http/Controllers/User/MyPageController.php
+++ src/app/Http/Controllers/User/MyPageController.php
@@ -19,9 +19,13 @@ class MyPageController extends Controller
         //Authのidに紐づいているテーブルを全部取得
         $event_organizes = Event::with('participants')->where('user_id', '=', $auth_id)->get();
         foreach ($event_organizes as $event_organize) {
-            print_r($event_organize->title);
-            print_r($event_organize->created_at);
-            print_r($event_organize->completed_at);
+            print_r($event_organize->title . '<br>');
+            print_r($event_organize->created_at . '<br>');
+            print_r($event_organize->completed_at . '<br>');
+        }
+        $earned_points = Event::where('user_id', $auth_id)->with('participants')->withSum('participants', 'point')->get();
+        foreach($earned_points as $earned_point){
+            print_r($earned_point->participants_sum_point);
         }
         dd();
         // return view();
