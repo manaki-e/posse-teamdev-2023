e345827fd310a0156925167d913625533daca7c8
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-05-07 08:25:01 +0000

    edit admin button component

--- src/resources/views/components/admin-button-delete.blade.php
+++ src/resources/views/components/admin-button-delete.blade.php
@@ -1,3 +1,7 @@
-<button class="middle none center mr-3 rounded-lg border border-red-700 py-3 px-6 font-sans text-xs font-bold uppercase text-red-700 transition-all hover:opacity-75 focus:ring focus:ring-red-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-dark="true">
-    削除
-</button>
+<form {{ $attributes }} method="post">
+    @csrf
+    @method('DELETE')
+    <button type="submit" class="middle none center mr-3 rounded-lg border border-red-700 py-3 px-6 font-sans text-xs font-bold uppercase text-red-700 transition-all hover:opacity-75 focus:ring focus:ring-red-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-dark="true">
+        削除
+    </button>
+</form>
