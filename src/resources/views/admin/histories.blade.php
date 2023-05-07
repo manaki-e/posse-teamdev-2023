7494fd26f65dacb4b222cf41e318fb1a30989d28
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-05-07 18:03:18 +0000

    fix admin histories

--- src/resources/views/admin/histories.blade.php
+++ src/resources/views/admin/histories.blade.php
@@ -11,10 +11,7 @@
             <div class="border-b border-b-gray-100">
                 <ul class="-mb-px flex items-center gap-4 text-sm font-medium">
                     <li>
-                        <a @click="activeTab = 0" class="inline-flex cursor-pointer items-center gap-2 px-1 py-3 text-blue-500 hover:text-blue-500" :class="{
-                                'relative text-blue-500  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:bg-blue-500': activeTab ===
-                                    0
-                            }">
+                        <a @click="activeTab = 0" class="inline-flex cursor-pointer items-center gap-2 px-1 py-3 text-blue-500 hover:text-blue-500" :class="{'relative text-blue-500  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:bg-blue-500': activeTab === 0}">
                             アイテム取引
                             <span class="rounded-full bg-gray-100 px-2 py-0.5 text-xs font-semibold text-gray-500">
                                 {{ $product_deals->total() }}
@@ -22,10 +19,7 @@
                         </a>
                     </li>
                     <li>
-                        <a @click="activeTab = 1" class="inline-flex cursor-pointer items-center gap-2 px-1 py-3 text-blue-500 hover:text-blue-500" :class="{
-                                'relative text-blue-500  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:bg-blue-500': activeTab ===
-                                    1
-                            }">
+                        <a @click="activeTab = 1" class="inline-flex cursor-pointer items-center gap-2 px-1 py-3 text-blue-500 hover:text-blue-500" :class="{'relative text-blue-500  after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:bg-blue-500': activeTab === 1}">
                             イベント参加
                             <span class="rounded-full bg-gray-100 px-2 py-0.5 text-xs font-semibold text-gray-500">
                                 {{ $event_participants->total() }}
@@ -52,20 +46,22 @@
                                 @foreach ( $product_deals as $product_deal )
                                 <tr class="hover:bg-gray-50">
                                     <td class="px-6 py-4">
-                                        <a href="/admin/items/{{ $product_deal -> product -> id }}" class="border-b border-blue-600 hover:text-blue-700">{{ $product_deal -> product -> title }}</a>
+                                        <a href="{{ route('admin.items.show', ['item' => $product_deal -> product -> id]) }}" class="border-b border-blue-600 hover:text-blue-700">{{ $product_deal -> product -> title }}</a>
                                     </td>
                                     <td class="px-6 py-4 text-right">{{ $product_deal -> product -> point }} pt</td>
                                     <td class="px-6 py-4">
-                                        <a href="/admin/users/{{ $product_deal -> user -> user_id }}" class="border-b border-blue-600 hover:text-blue-700">{{ $product_deal -> user -> name }}</a>
+                                        <a href="{{ route('admin.users.show', ['user' => $product_deal -> user -> id]) }}" class="border-b border-blue-600 hover:text-blue-700">{{ $product_deal -> user -> name }}</a>
                                     </td>
                                     <td class="px-6 py-4">
-                                        <a href="/admin/users/{{ $product_deal -> product -> user -> user_id }}" class="border-b border-blue-600 hover:text-blue-700">{{ $product_deal -> product -> user -> name }}</a>
+                                        <a href="{{ route('admin.users.show', ['user' => $product_deal -> product -> user -> id]) }}" class="border-b border-blue-600 hover:text-blue-700">{{ $product_deal -> product -> user -> name }}</a>
                                     </td>
                                     <td class="px-6 py-4 text-right">
                                         {{ date( 'Y年m月d日 H時i分s秒', strtotime( $product_deal -> created_at ) ) }}
                                     </td>
                                     <td class="px-6 py-4 text-right">
-                                        {{ date( 'Y年m月d日 H時i分s秒', strtotime( $product_deal -> returned_at ) ) }}
+                                        {{ $product_deal -> returned_at
+                                        ? date( 'Y年m月d日 H時i分s秒', strtotime( $product_deal -> returned_at ) )
+                                        : '未返却' }}
                                     </td>
                                 </tr>
                                 @endforeach
@@ -89,9 +85,10 @@
                             <tbody class="divide-y divide-gray-100 border-t border-gray-100">
                                 @foreach ( $event_participants as $event_participant )
                                 <tr class="hover:bg-gray-50">
+                                    <!-- 後ほど修正する -->
                                     <th class="px-6 py-4 font-medium text-gray-900">{{ $event_participant -> event -> title }}</th>
                                     <td class="px-6 py-4">
-                                        <a href="/admin/users/{{ $event_participant -> user -> user_id }}" class="border-b border-blue-600 hover:text-blue-700">{{ $event_participant -> user -> name }}</a>
+                                        <a href="{{ route('admin.users.show', ['user' => $event_participant -> user -> id]) }}" class="border-b border-blue-600 hover:text-blue-700">{{ $event_participant -> user -> name }}</a>
                                     </td>
                                     <td class="px-6 py-4 font-medium text-gray-900 text-right">{{ $event_participant -> point }} pt</td>
                                     <td class=" px-6 py-4 text-right">
