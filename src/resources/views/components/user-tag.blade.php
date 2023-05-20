6e39038d857962d0257a5e8ea3145ca07dbc1027
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-05-20 13:22:42 +0000

    edit tag css

--- src/resources/views/components/user-tag.blade.php
+++ src/resources/views/components/user-tag.blade.php
@@ -1,1 +1,1 @@
-<span class="rounded-full bg-gray-300 px-3 py-1 text-xs font-semibold text-gray-600">{{ $slot }}</span>
+<span class="inline-flex items-center gap-1 rounded-full border border-gray-300 bg-gray-50 px-2 py-1 text-xs font-semibold text-gray-400">{{ $slot }}</span>
