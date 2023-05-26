0788c3c2da49ba915da5e2a46cd30c0190ff96de
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-05-26 13:46:33 +0900

    Update user-header.blade.php

--- src/resources/views/components/user-header.blade.php
+++ src/resources/views/components/user-header.blade.php
@@ -84,7 +84,7 @@ $user_info = Auth::user();
                             <ul>
                                 <li class="px-5 py-3 font-medium hover:bg-gray-100">
                                     <!-- プロフィールに移行する -->
-                                    <a href="{{ route('profile.edit') }}" class="flex items-center transform transition-colors duration-200">
+                                    <a href="#" class="flex items-center transform transition-colors duration-200">
                                         <div class="mr-3">
                                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z">
