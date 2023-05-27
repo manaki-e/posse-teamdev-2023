2ac68ec81fa93d9aeb32ae57bee063365e3190d0
soh-github <94168577+soh-github@users.noreply.github.com>
2023-05-27 10:26:53 +0000

    sidenaviのhover削除

--- src/resources/views/components/user-side-navi.blade.php
+++ src/resources/views/components/user-side-navi.blade.php
@@ -24,7 +24,7 @@
                             <x-icon-pps></x-icon-pps>
                         </div>
                         <p class="font-patua" :class=" (isSidebarOpen) ? 'hidden': 'text-xs'">Product</p>
-                        <p x-cloak class="font-patua p-1 rounded hover:bg-gray-100" :class="{ 'hidden' : ! isSidebarOpen, 'ml-2 text-base' : isSidebarOpen}">Peer Product
+                        <p x-cloak class="font-patua" :class="{ 'hidden' : ! isSidebarOpen, 'ml-2 text-base' : isSidebarOpen}">Peer Product
                             Share</p>
                     </div>
                 </a>
@@ -35,7 +35,7 @@
                             <x-icon-pe></x-icon-pe>
                         </div>
                         <p class="font-patua" :class=" (isSidebarOpen) ? 'hidden': 'text-xs'">Event</p>
-                        <p x-cloak class="font-patua p-1 rounded hover:bg-gray-100" :class="{'ml-2 text-base' : isSidebarOpen, 'hidden' : ! isSidebarOpen}">Peer Event</p>
+                        <p x-cloak class="font-patua" :class="{'ml-2 text-base' : isSidebarOpen, 'hidden' : ! isSidebarOpen}">Peer Event</p>
                     </div>
                 </a>
                 <!-- Request -->
@@ -46,13 +46,13 @@
                             <x-icon-pr></x-icon-pr>
                         </div>
                         <p class="font-patua" :class=" (isSidebarOpen) ? 'hidden': 'text-xs'">Request</p>
-                        <p x-cloak class="font-patua p-1 rounded hover:bg-gray-100" :class="{ 'hidden' : ! isSidebarOpen, 'ml-2 text-base' : isSidebarOpen}">Peer Request</p>
+                        <p x-cloak class="font-patua" :class="{ 'hidden' : ! isSidebarOpen, 'ml-2 text-base' : isSidebarOpen}">Peer Request</p>
                     </div>
                 </a>
                 <!-- アプリ内リンク -->
                 <div x-show="isSidebarOpen">
                     <nav x-cloak x-show="isSidebarOpen" aria-label="Main" class="flex flex-col">
-                        <hr class="border-gray-500 mx-4">
+                        <hr class="border-gray-500 mx-4 mb-2">
 
                         <!-- urlが/eventsまたは/events/*の場合 -->
                         @if (request()->is('events') || request()->is('events/*'))
@@ -60,7 +60,7 @@
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 21C4.45 21 3.979 20.804 3.587 20.412C3.195 20.02 2.99934 19.5493 3 19V5C3 4.45 3.196 3.979 3.588 3.587C3.98 3.195 4.45067 2.99934 5 3H19C19.55 3 20.021 3.196 20.413 3.588C20.805 3.98 21.0007 4.45067 21 5V19C21 19.55 20.804 20.021 20.412 20.413C20.02 20.805 19.5493 21.0007 19 21H15V19H19V7H5V19H9V21H5ZM11 21V14.85L9.4 16.45L8 15L12 11L16 15L14.6 16.45L13 14.85V21H11Z" fill="black" />
                             </svg>
-                            <p class="font-patua ml-2 p-1 text-sm rounded hover:bg-gray-100">
+                            <p class="font-patua ml-2 p-1 text-sm">
                                 参加イベント
                             </p>
                         </a>
@@ -69,7 +69,7 @@
                                 <path d="M19.7151 0.855469H4.28655C3.33978 0.855469 2.57227 1.62298 2.57227 2.56975V21.4269C2.57227 22.3737 3.33978 23.1412 4.28655 23.1412H19.7151C20.6619 23.1412 21.4294 22.3737 21.4294 21.4269V2.56975C21.4294 1.62298 20.6619 0.855469 19.7151 0.855469Z" stroke="black" stroke-width="1.71429" stroke-linecap="round" stroke-linejoin="round" />
                                 <path d="M2.57227 17.9978H21.4294M7.71512 5.14062H16.2866M7.71512 9.42634H16.2866M7.71512 13.7121H12.858" stroke="black" stroke-width="1.71429" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
-                            <p class="font-patua ml-2 p-1 text-sm rounded hover:bg-gray-100">
+                            <p class="font-patua ml-2 p-1 text-sm">
                                 主催イベント
                             </p>
                         </a>
@@ -80,7 +80,7 @@
                                 <path d="M19.7151 0.855469H4.28655C3.33978 0.855469 2.57227 1.62298 2.57227 2.56975V21.4269C2.57227 22.3737 3.33978 23.1412 4.28655 23.1412H19.7151C20.6619 23.1412 21.4294 22.3737 21.4294 21.4269V2.56975C21.4294 1.62298 20.6619 0.855469 19.7151 0.855469Z" stroke="black" stroke-width="1.71429" stroke-linecap="round" stroke-linejoin="round" />
                                 <path d="M2.57227 17.9978H21.4294M7.71512 5.14062H16.2866M7.71512 9.42634H16.2866M7.71512 13.7121H12.858" stroke="black" stroke-width="1.71429" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
-                            <p class="font-patua ml-2 p-1 text-sm rounded hover:bg-gray-100">
+                            <p class="font-patua ml-2 p-1 text-sm">
                                 出品一覧
                             </p>
                         </a>
@@ -89,7 +89,7 @@
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M4.5 12C4.5 16.142 7.858 19.5 12 19.5C16.142 19.5 19.5 16.142 19.5 12C19.5 7.85801 16.142 4.50001 12 4.50001C10.0458 4.49738 8.16819 5.26018 6.7695 6.62501H8.375C8.57391 6.62501 8.76468 6.70403 8.90533 6.84468C9.04598 6.98533 9.125 7.1761 9.125 7.37501C9.125 7.57392 9.04598 7.76469 8.90533 7.90534C8.76468 8.04599 8.57391 8.12501 8.375 8.12501H5.125C4.92609 8.12501 4.73532 8.04599 4.59467 7.90534C4.45402 7.76469 4.375 7.57392 4.375 7.37501V4.12501C4.375 3.9261 4.45402 3.73533 4.59467 3.59468C4.73532 3.45403 4.92609 3.37501 5.125 3.37501C5.32391 3.37501 5.51468 3.45403 5.65533 3.59468C5.79598 3.73533 5.875 3.9261 5.875 4.12501V5.40551C7.5381 3.85653 9.72728 2.99676 12 3.00001C16.9705 3.00001 21 7.02951 21 12C21 16.9705 16.9705 21 12 21C7.0295 21 3 16.9705 3 12C3 11.7395 3.011 11.481 3.033 11.226C3.0685 10.805 3.434 10.5 3.8565 10.5C4.2665 10.5 4.5725 10.8765 4.5335 11.285C4.5115 11.52 4.5 11.758 4.5 12ZM12.5 7.75001C12.5 7.5511 12.421 7.36033 12.2803 7.21968C12.1397 7.07903 11.9489 7.00001 11.75 7.00001C11.5511 7.00001 11.3603 7.07903 11.2197 7.21968C11.079 7.36033 11 7.5511 11 7.75001V12.75C11 12.9489 11.079 13.1397 11.2197 13.2803C11.3603 13.421 11.5511 13.5 11.75 13.5H14.25C14.4489 13.5 14.6397 13.421 14.7803 13.2803C14.921 13.1397 15 12.9489 15 12.75C15 12.5511 14.921 12.3603 14.7803 12.2197C14.6397 12.079 14.4489 12 14.25 12H12.5V7.75001Z" fill="black" />
                             </svg>
-                            <p class="font-patua ml-2 p-1 text-sm rounded hover:bg-gray-100">
+                            <p class="font-patua ml-2 p-1 text-sm">
                                 貸借履歴
                             </p>
                         </a>
@@ -100,7 +100,7 @@
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 21C4.45 21 3.979 20.804 3.587 20.412C3.195 20.02 2.99934 19.5493 3 19V5C3 4.45 3.196 3.979 3.588 3.587C3.98 3.195 4.45067 2.99934 5 3H19C19.55 3 20.021 3.196 20.413 3.588C20.805 3.98 21.0007 4.45067 21 5V19C21 19.55 20.804 20.021 20.412 20.413C20.02 20.805 19.5493 21.0007 19 21H15V19H19V7H5V19H9V21H5ZM11 21V14.85L9.4 16.45L8 15L12 11L16 15L14.6 16.45L13 14.85V21H11Z" fill="black" />
                             </svg>
-                            <p class="font-patua ml-2 p-1 text-sm rounded hover:bg-gray-100">
+                            <p class="font-patua ml-2 p-1 text-sm">
                                 投稿したリクエスト
                             </p>
                         </a>
