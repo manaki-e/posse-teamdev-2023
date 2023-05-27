2782df645f183debdd90717159d9bc7d88200094
Yuta0227 <yuta0227.msc28@gmail.com>
2023-05-27 09:12:51 +0000

    tagできた

--- src/database/migrations/2023_04_24_120958_create_tags_table.php
+++ src/database/migrations/2023_04_24_120958_create_tags_table.php
@@ -20,6 +20,39 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+        
+        $tags_array = [
+            ['request_type_id' => '1', 'name' => 'モニター'],
+            ['request_type_id' => '1', 'name' => 'キーボード'],
+            ['request_type_id' => '1', 'name' => 'マウス'],
+            ['request_type_id' => '1', 'name' => 'イヤホン'],
+            ['request_type_id' => '1', 'name' => 'デスク'],
+            ['request_type_id' => '1', 'name' => 'スタンド'],
+            ['request_type_id' => '1', 'name' => 'ヘルスケア'],
+            ['request_type_id' => '1', 'name' => 'マイク'],
+            ['request_type_id' => '1', 'name' => 'スピーカー'],
+            ['request_type_id' => '1', 'name' => '充電器'],
+            ['request_type_id' => '1', 'name' => 'ケーブル'],
+            ['request_type_id' => '1', 'name' => 'モバイルバッテリー'],
+            ['request_type_id' => '1', 'name' => 'PC'],
+            ['request_type_id' => '1', 'name' => 'タブレット'],
+            ['request_type_id' => '2', 'name' => 'PHP'],
+            ['request_type_id' => '2', 'name' => 'MySQL'],
+            ['request_type_id' => '2', 'name' => 'JavaScript'],
+            ['request_type_id' => '2', 'name' => 'Python'],
+            ['request_type_id' => '2', 'name' => 'Django'],
+            ['request_type_id' => '2', 'name' => 'Ruby'],
+            ['request_type_id' => '2', 'name' => 'Java'],
+            ['request_type_id' => '2', 'name' => 'C#'],
+            ['request_type_id' => '2', 'name' => 'NoSQL'],
+            ['request_type_id' => '2', 'name' => 'C'],
+            ['request_type_id' => '2', 'name' => 'ゲーム大会'],
+            ['request_type_id' => '2', 'name' => 'バーベキュー'],
+            ['request_type_id' => '2', 'name' => 'カラオケ大会'],
+            ['request_type_id' => '2', 'name' => '飲み会'],
+            ['request_type_id' => '2', 'name' => 'シャッフルランチ'],
+        ];
+        DB::table('tags')->insert($tags_array);
     }
 
     /**
