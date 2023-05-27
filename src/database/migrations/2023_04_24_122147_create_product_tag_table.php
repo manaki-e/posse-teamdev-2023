09b9f6610f3af05231333cc78fe853d6b49e5a12
Yuta0227 <yuta0227.msc28@gmail.com>
2023-05-27 15:54:08 +0000

    product_tagsできた

--- src/database/migrations/2023_04_24_122147_create_product_tag_table.php
+++ src/database/migrations/2023_04_24_122147_create_product_tag_table.php
@@ -20,6 +20,32 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        $product_tags_array = [
+            ///macbook 1
+            ['product_id' => 1, 'tag_id' => 13],
+            //logicool mouse 2
+            ['product_id' => 2, 'tag_id' => 3],
+            //logicool 3
+            ['product_id' => 3, 'tag_id' => 2],
+            //airpods 4
+            ['product_id' => 4, 'tag_id' => 4],
+            //anker727 5
+            ['product_id' => 5, 'tag_id' => 10],
+            //flexispot 6
+            ['product_id' => 6, 'tag_id' => 5],
+            //anker511 7
+            ['product_id' => 7, 'tag_id' => 10],
+            ['product_id' => 7, 'tag_id' => 12],
+            //satechi 8
+            ['product_id' => 8, 'tag_id' => 6],
+            //Shokz OpenRun Pro 9
+            ['product_id' => 9, 'tag_id' => 4],
+            //galaxy watch 10
+            ['product_id' => 10, 'tag_id' => 7],
+        ];
+
+        DB::table('product_tag')->insert($product_tags_array);
     }
 
     /**
