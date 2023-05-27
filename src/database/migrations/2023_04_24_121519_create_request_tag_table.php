ec0be1e8ffe083bd938d63e61b30a4b37424846f
Yuta0227 <yuta0227.msc28@gmail.com>
2023-05-27 16:11:40 +0000

    request_tagsできた

--- src/database/migrations/2023_04_24_121519_create_request_tag_table.php
+++ src/database/migrations/2023_04_24_121519_create_request_tag_table.php
@@ -20,6 +20,24 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        $request_tag_array = [
+            // バッテリー
+            ['request_id' => 1, 'tag_id' => 12],
+            // PHP
+            ['request_id' => 2, 'tag_id' => 15],
+            // tailwindタグなし
+            //macbook
+            ['request_id' => 4, 'tag_id' => 13],
+            //rustはタグなし
+            //ruby on rails
+            ['request_id' => 6, 'tag_id' => 20],
+            // airpods
+            ['request_id' => 7, 'tag_id' => 4],
+            //キーボード
+            ['request_id' => 8, 'tag_id' => 2],
+        ];
+        DB::table('request_tag')->insert($request_tag_array);
     }
 
     /**
