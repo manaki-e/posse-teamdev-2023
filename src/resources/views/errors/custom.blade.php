41745fa31754cb3e01a41a6364f6da39910ef550
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-05-28 12:23:51 +0000

    laravelのエラー表示が出ないようにした

---
+++ src/resources/views/errors/custom.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Error</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 26px;
+            margin-bottom: 10px;
+        }
+
+        p {
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+
+        a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 4px;
+        }
+
+        a:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>エラーが発生しました</h1>
+    <p>申し訳ありませんが、何らかの不具合が生じました。</p>
+    <a href="/items">トップページに戻る</a>
+</body>
+
+</html>
