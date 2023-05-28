3bf93133278809ccaf91a2b733faf9dad06ba160
soh-github <94168577+soh-github@users.noreply.github.com>
2023-05-28 07:43:39 +0000

    ヘッダーのマイページ関連リンク反映

--- src/resources/views/components/user-header.blade.php
+++ src/resources/views/components/user-header.blade.php
@@ -92,7 +92,7 @@ $user_info = Auth::user();
                             <ul>
                                 <li class="px-5 py-3 font-medium hover:bg-gray-100">
                                     <!-- プロフィールに移行する -->
-                                    <a href="#" class="flex items-center transform transition-colors duration-200">
+                                    <a href="{{ route('mypage.profile') }}" class="flex items-center transform transition-colors duration-200">
                                         <div class="mr-3">
                                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z">
@@ -103,7 +103,7 @@ $user_info = Auth::user();
                                     </a>
                                 </li>
                                 <li class="px-5 py-3 font-medium hover:bg-gray-100">
-                                    <a href="#" class="flex items-center transform transition-colors duration-200">
+                                    <a href="{{ route('mypage.account') }}" class="flex items-center transform transition-colors duration-200">
                                         <div class="mr-3">
                                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z">
