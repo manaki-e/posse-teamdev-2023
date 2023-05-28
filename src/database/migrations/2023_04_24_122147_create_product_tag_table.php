0570e2196328e263d108c90d3c766d204223fec2
Yuta0227 <yuta0227.msc28@gmail.com>
2023-05-28 04:42:27 +0000

    product_tagに反映

--- src/database/migrations/2023_04_24_122147_create_product_tag_table.php
+++ src/database/migrations/2023_04_24_122147_create_product_tag_table.php
@@ -23,26 +23,28 @@ return new class extends Migration
 
         $product_tags_array = [
             ///macbook 1
-            ['product_id' => 1, 'tag_id' => 13],
+            ['product_id' => 1, 'tag_id' => 9],
+            ['product_id' => 1, 'tag_id' => 2],
             //logicool mouse 2
-            ['product_id' => 2, 'tag_id' => 3],
+            ['product_id' => 2, 'tag_id' => 4],
+            ['product_id' => 2, 'tag_id' => 2],
             //logicool 3
+            ['product_id' => 3, 'tag_id' => 3],
             ['product_id' => 3, 'tag_id' => 2],
             //airpods 4
-            ['product_id' => 4, 'tag_id' => 4],
+            ['product_id' => 4, 'tag_id' => 5],
             //anker727 5
-            ['product_id' => 5, 'tag_id' => 10],
+            ['product_id' => 5, 'tag_id' => 2],
             //flexispot 6
-            ['product_id' => 6, 'tag_id' => 5],
+            ['product_id' => 6, 'tag_id' => 6],
             //anker511 7
-            ['product_id' => 7, 'tag_id' => 10],
-            ['product_id' => 7, 'tag_id' => 12],
+            ['product_id' => 7, 'tag_id' => 2],
             //satechi 8
             ['product_id' => 8, 'tag_id' => 6],
             //Shokz OpenRun Pro 9
-            ['product_id' => 9, 'tag_id' => 4],
+            ['product_id' => 9, 'tag_id' => 5],
             //galaxy watch 10
-            ['product_id' => 10, 'tag_id' => 7],
+            ['product_id' => 10, 'tag_id' => 8],
         ];
 
         DB::table('product_tag')->insert($product_tags_array);
