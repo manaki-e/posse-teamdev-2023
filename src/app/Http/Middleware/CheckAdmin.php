b552f7dde53ee86bf9fc3823c1af57d97862239c
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-06-24 08:52:00 +0000

    不要なコード消したりした

--- src/app/Http/Middleware/CheckAdmin.php
+++ src/app/Http/Middleware/CheckAdmin.php
@@ -20,19 +20,16 @@ class CheckAdmin
         $previous_url = str_replace(config('app.url'), '', url()->previous());
         //一般ユーザーがユーザー画面から管理者画面に遷移しようとしている場合
         if (strpos($previous_url, 'admin') !== 0 && auth()->user()->is_admin === 0) {
-            return redirect()->back()->with(['flush.message' => '管理者のみアクセスできます。', 'flush.alert_type' => 'error']);
+            return redirect()->back()->with(['flush.message' => '管理者のみアクセスできます。', 'flush.alert_type' => 'warning']);
         }
         $response = $next($request);
-        if (strpos($previous_url, 'admin') === 0&& $request->method()==='GET') {
+        if (strpos($previous_url, 'admin') === 0 && $request->method() === 'GET') {
             //ログインしている管理者が自分を一般ユーザーに変更した処理の後
             if (auth()->user()->is_admin === 0) {
                 auth()->logout();
-                return redirect()->route('login')->with(['flush.message' => '自分のアカウントを一般ユーザーに変えました。ログインしなおしてください。', 'flush.alert_type' => 'success']);
             }
-            //ログインしている管理者が自分を削除した処理の後
-            elseif(!empty(auth()->user()->deleted_at)){
-                return redirect()->route('login')->with(['flush.message' => '自分のアカウント削除に成功しました。', 'flush.alert_type' => 'success']);
-            }
+            //ログインしている管理者が自分を削除した処理の後何もしない
+            return redirect()->route('login');
         }
         return $response;
     }
