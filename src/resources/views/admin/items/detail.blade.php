a45e2fb4ff8918a2f45741c6f0f108aae297a42d
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-06-24 04:34:46 +0000

    アイテムのポイント設定と再設定に上限儲けた

--- src/resources/views/admin/items/detail.blade.php
+++ src/resources/views/admin/items/detail.blade.php
@@ -155,7 +155,7 @@
                                     <div class="mb-4">
                                         <div class="relative flex gap-4">
                                             <label for="point" class="leading-7 text-sm text-gray-600 flex-center">Point:</label>
-                                            <input type="number" id="point" name="point" class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
+                                            <input max="5000" type="number" id="point" name="point" class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
                                         </div>
                                     </div>
                                 </x-slot>
@@ -180,7 +180,7 @@
                                     <div class="mb-4">
                                         <div class="relative flex gap-4">
                                             <label for="point" class="leading-7 text-sm text-gray-600 flex-center">Point:</label>
-                                            <input type="number" id="point" name="point" value="{{ $product -> point }}" class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            <input max="5000" type="number" id="point" name="point" value="{{ $product -> point }}" class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                         </div>
                                     </div>
                                 </x-slot>
--- src/resources/views/admin/items/index.blade.php
+++ src/resources/views/admin/items/index.blade.php
@@ -88,7 +88,7 @@
                                                     <div class="relative flex gap-4">
                                                         <label for="point"
                                                             class="leading-7 text-sm text-gray-600 flex-center">Point:</label>
-                                                        <input type="number" id="point" name="point"
+                                                        <input max="5000" type="number" id="point" name="point"
                                                             value="{{ $not_pending_product -> point }}"
                                                             class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                     </div>
@@ -160,7 +160,7 @@
                                                     <div class="relative flex gap-4">
                                                         <label for="point"
                                                             class="leading-7 text-sm text-gray-600 flex-center">Point:</label>
-                                                        <input type="number" id="point" name="point"
+                                                        <input max="5000" type="number" id="point" name="point"
                                                             class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
                                                     </div>
                                                 </div>
