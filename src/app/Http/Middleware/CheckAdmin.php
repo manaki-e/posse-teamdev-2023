77441416f5b5511e5cd6d957279360fd836aac11
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-06-25 08:06:11 +0000

    不要な条件分岐削除

--- src/app/Http/Middleware/CheckAdmin.php
+++ src/app/Http/Middleware/CheckAdmin.php
@@ -17,10 +17,7 @@ class CheckAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        //get previous url without APP_URL
-        $previous_url = str_replace(config('app.url'), '', url()->previous());
-        //一般ユーザーがユーザー画面から管理者画面に遷移しようとしている場合
-        if (strpos($previous_url, 'admin') !== 0 && auth()->user()->is_admin === 0) {
+        if (!auth()->user()->is_admin) {
             return redirect()->back()->with(['flush.message' => '管理者のみアクセスできます。', 'flush.alert_type' => 'warning']);
         }
         return $next($request);
