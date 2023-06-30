03a91d1e3f73eca8f368d4c7ebb5995e00ea2ac6
soh-github <94168577+soh-github@users.noreply.github.com>
2023-06-30 05:33:00 +0000

    画像のアップロード上限を３枚に設定

--- src/resources/views/user/items/create.blade.php
+++ src/resources/views/user/items/create.blade.php
@@ -27,7 +27,7 @@
                                             または ファイルをドロップ</div>
                                         <p class="text-sm text-gray-500">SVG, PNG or JPG (max. 1MB, 3枚まで)</p>
                                     </div>
-                                    <input id="file" type="file" name="product_images[]" class="sr-only" multiple required onchange="checkFileSize(this),preview(this)" />
+                                    <input id="file" type="file" name="product_images[]" class="sr-only" multiple required onchange="checkFileSize(this),handleImageUpload(this),preview(this)" />
                                     <div class="preview-area "></div>
                                 </label>
                             </div>
@@ -138,13 +138,22 @@
     function checkFileSize(input) {
         const maxSize = 1 * 1024 * 1024; // 1MB in bytes
         const files = input.files;
-        let totalSize=Array.from(files).reduce((accumulator,file)=>accumulator+file.size,0);
-        const totalSizeInMB=totalSize/(1024*1024);
-        if(totalSizeInMB>1){
+        let totalSize = Array.from(files).reduce((accumulator, file) => accumulator + file.size, 0);
+        const totalSizeInMB = totalSize / (1024 * 1024);
+        if (totalSizeInMB > 1) {
             alert('合計ファイルサイズが1MBを超えています。');
-            input.value=null;
+            input.value = null;
         }
-        // Proceed with file preview or submission
-        preview(input);
+    }
+
+    function handleImageUpload(input) {
+        const files = input.files;
+
+        if (files.length > 3) {
+            // 選択されたファイルが3枚を超える場合はアラートを表示
+            alert('最大3枚までの画像を選択してください');
+            // 選択されたファイルをリセット
+            input.value = null;
+        }
     }
 </script>
