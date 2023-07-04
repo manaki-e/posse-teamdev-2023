c6acd5d33e01649f71511c09eb06b2a6380b6093
Yuta0227 <86890087+Yuta0227@users.noreply.github.com>
2023-07-04 03:53:27 +0000

    利用ポイントからポイントに名前変更

--- src/resources/views/admin/histories.blade.php
+++ src/resources/views/admin/histories.blade.php
@@ -35,7 +35,7 @@
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">商品名</th>
-                                    <th scope="col" class="px-6 py-4 font-medium text-gray-900 text-right">利用ポイント</th>
+                                    <th scope="col" class="px-6 py-4 font-medium text-gray-900 text-right">ポイント</th>
                                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">借用者氏名</th>
                                     <th scope="col" class="px-6 py-4 font-medium text-gray-900">貸出者氏名</th>
                                     <th scope="col" class="px-6 py-4 font-medium text-gray-900 text-right">貸出日時</th>
