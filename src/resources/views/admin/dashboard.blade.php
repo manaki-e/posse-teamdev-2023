8e2b80f2ba29908a2e17359f3226ff4c66b6096b
Manaki Endo <94539342+manaki-e@users.noreply.github.com>
2023-07-11 03:04:22 +0000

    delete unnecessary part

--- src/resources/views/admin/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-<x-admin-app>
-    <x-slot name="title">
-            {{ __('ダッシュボード') }}
-    </x-slot>
-    <x-slot name="description">
-            {{ __('ダッシュボード') }}
-    </x-slot>
-</x-admin-app>
--- src/routes/web.php
+++ src/routes/web.php
@@ -28,10 +28,6 @@ Route::get('/', function () {
     return Redirect::to('/login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
